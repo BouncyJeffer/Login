e13c4caeea0773bc941e6eed61fe9c1f84ea78c4
BouncyJeffer <BouncyJeffer@gmail.com>
2016-08-27 22:54:30 -0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>Login</title>
+</head>
+
+<!-- Enter your CSS to modify your login -->
+<style>
+
+div#body {
+	text-align: center;
+	font-size: 30;
+}
+
+</style>
+
+<!-- Main Login Section -->
+<body>
+<div id="body">
+<p>Welcome To Our Website, Please Login.</p>
+<i class="large material-icons">insert_chart</i>
+<form action="login.php" method="POST">
+<p><b>Username</b></p><input type="text" name="user" placeholder="Username">
+<p><b>Password</b></p><input type="password" name="pass" placeholder="Password">
+<br><input type="submit" value="Login">
+</body>
+</div>
+
+</html>
+
